b4be45accb74015298fdef04835b068ebf20db53
EricWehrly <eric@disruptiontheory.com>
2022-08-16 17:14:14 -0400

    working (mostly) module for subdomain

--- main.tf
+++ main.tf
@@ -1,27 +1,4 @@
-variable "domain" {
-    default = "wehrly.com"
-    type = string
-}
-
-provider "aws" {
-  region = "us-east-1"
-}
-
-module "cdn" {
-  # source = "cloudposse/cloudfront-s3-cdn/aws"
-  source = "git::https://github.com/cloudposse/terraform-aws-cloudfront-s3-cdn.git?ref=0.82.4"
-  # Cloud Posse recommends pinning every module to a specific version
-  # version = "x.x.x"
-
-  # namespace         = "eg"
-  stage             = "prod"
-  name              = "app"
-  # aliases           = ["assets.cloudposse.com"]
-  dns_alias_enabled = true
-  parent_zone_name  = var.domain
-
-  #deployment_principal_arns = {
-    #"arn:aws:iam::123456789012:role/principal1" = ["prefix1/", "prefix2/"]
-    #"arn:aws:iam::123456789012:role/principal2" = [""]
-  #}
+module "www-site" {
+  source = "./modules/s3-site"
+  domain = "www.wehrly.com"
 }
---
+++ modules/s3-site/main.tf
@@ -0,0 +1,116 @@
+# https://gist.github.com/danihodovic/a51eb0d9d4b29649c2d094f4251827dd
+
+variable "aws_region" {
+  type = string
+  default = "us-east-1"
+}
+
+variable "domain" {
+  type = string
+}
+
+provider "aws" {
+  region = "${var.aws_region}"
+}
+
+# Note: The bucket name needs to carry the same name as the domain!
+# http://stackoverflow.com/a/5048129/2966951
+resource "aws_s3_bucket" "site" {
+  bucket = "${var.domain}"
+  acl = "public-read"
+
+  policy = <<EOF
+    {
+      "Version":"2008-10-17",
+      "Statement":[{
+        "Sid":"AllowPublicRead",
+        "Effect":"Allow",
+        "Principal": {"AWS": "*"},
+        "Action":["s3:GetObject"],
+        "Resource":["arn:aws:s3:::${var.domain}/*"]
+      }]
+    }
+  EOF
+
+  website {
+      index_document = "index.html"
+  }
+}
+
+# Note: Creating this route53 zone is not enough. The domain's name servers need to point to the NS
+# servers of the route53 zone. Otherwise the DNS lookup will fail.
+# To verify that the dns lookup succeeds: `dig site @nameserver`
+resource "aws_route53_zone" "main" {
+  name = "wehrly.com"
+}
+
+resource "aws_route53_record" "root_domain" {
+  zone_id = "${aws_route53_zone.main.zone_id}"
+  name = "${var.domain}"
+  type = "A"
+
+  alias {
+    name = "${aws_cloudfront_distribution.cdn.domain_name}"
+    zone_id = "${aws_cloudfront_distribution.cdn.hosted_zone_id}"
+    evaluate_target_health = false
+  }
+}
+
+resource "aws_cloudfront_distribution" "cdn" {
+  origin {
+    origin_id   = "${var.domain}"
+    domain_name = "${var.domain}.s3.amazonaws.com"
+  }
+
+  # If using route53 aliases for DNS we need to declare it here too, otherwise we'll get 403s.
+  aliases = ["${var.domain}"]
+
+  enabled             = true
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "${var.domain}"
+
+    forwarded_values {
+      query_string = true
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "allow-all"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  # The cheapest priceclass
+  price_class = "PriceClass_100"
+
+  # This is required to be specified even if it's not used.
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+      locations        = []
+    }
+  }
+
+  viewer_certificate {
+    acm_certificate_arn = "arn:aws:acm:us-east-1:428933486948:certificate/1f1c2305-429b-4889-8b08-77ce681f18b1"
+    ssl_support_method = "sni-only"
+  }
+}
+
+output "s3_website_endpoint" {
+  value = "${aws_s3_bucket.site.website_endpoint}"
+}
+
+output "route53_domain" {
+  value = "${aws_route53_record.root_domain.fqdn}"
+}
+
+output "cdn_domain" {
+  value = "${aws_cloudfront_distribution.cdn.domain_name}"
+}
