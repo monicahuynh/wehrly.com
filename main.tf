099f8b7b52b6100b376acfd9fee1978ebc268b68
Eric Wehrly <eric.wehrly@gmail.com>
2024-04-05 21:22:41 -0400

    upload supporting files via terraform

--- main.tf
+++ main.tf
@@ -13,6 +13,7 @@ module "wedding-site" {
   domain = "wedding.wehrly.com"
 }
 
+/*
 resource "aws_s3_object" "wedding_index" {
   bucket = module.wedding-site.bucket_id
   key    = "index.html"
@@ -21,4 +22,32 @@ resource "aws_s3_object" "wedding_index" {
   content_type = "text/html"
 
   etag = filemd5("wedding/index.html")
+}
+*/
+
+module "template_files" {
+  source = "hashicorp/dir/template"
+
+  base_dir = "${path.module}/wedding"
+  template_vars = {
+    # Pass in any values that you wish to use in your templates.
+  }
+}
+
+resource "aws_s3_object" "static_files" {
+  for_each = module.template_files.files
+
+  bucket       = module.wedding-site.bucket_id
+  key          = each.key
+  content_type = each.value.content_type
+
+  # The template_files module guarantees that only one of these two attributes
+  # will be set for each file, depending on whether it is an in-memory template
+  # rendering result or a static file on disk.
+  source  = each.value.source_path
+  content = each.value.content
+
+  # Unless the bucket has encryption enabled, the ETag of each object is an
+  # MD5 hash of that object.
+  etag = each.value.digests.md5
 }
