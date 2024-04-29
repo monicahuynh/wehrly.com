679b3a8e24a51354a25a3621ab089bb1b0a01ed6
Eric Wehrly <eric.wehrly@gmail.com>
2024-04-29 16:31:02 -0400

    make the menu pop down on mobile

--- wedding/js/edub.js
+++ wedding/js/edub.js
@@ -1,5 +1,21 @@
+function toggleClass(element, className) {
+    if(element.classList.contains(className)) {
+        element.classList.remove(className);
+    } else {
+        element.classList.add(className);
+    }
+}
 
-(function () {
+function stupidMobileMenu() {
+
+    const button = document.querySelector('.css-bcsx0a');
+    const mobileMenu = document.querySelector('.menu.mobile');
+    button.addEventListener("click", (event) => {
+        toggleClass(mobileMenu, 'open');
+    });
+}
+
+function stickyHeader() {    
     const headers = document.getElementsByClassName("edub-header");
 
     if (headers && headers.length > 0) {
@@ -15,4 +31,11 @@
     } else {
         console.warn(headers)
     }
+}
+
+(function () {
+    // stupidMobileMenu();
+    // stickyHeader();
 })();
+
+setTimeout(stupidMobileMenu, 500);
