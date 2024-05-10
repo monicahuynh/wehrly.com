d028d8a42ca5237bf6053e7d86cec5f25767a0f5
Eric Wehrly <1851833+EricWehrly@users.noreply.github.com>
2024-05-10 01:59:26 -0400

    remove unused policy definition

--- modules/s3-site/main.tf
+++ modules/s3-site/main.tf
@@ -18,24 +18,10 @@ provider "aws" {
   region = "${var.aws_region}"
 }
 
-# The below probably needs to get deleted, but we also need to not mess with something that works right now.
 # Note: The bucket name needs to carry the same name as the domain!
 # http://stackoverflow.com/a/5048129/2966951
 resource "aws_s3_bucket" "site" {
   bucket = "${var.domain}"
-
-  policy = <<EOF
-    {
-      "Version":"2008-10-17",
-      "Statement":[{
-        "Sid":"AllowPublicRead",
-        "Effect":"Allow",
-        "Principal": {"AWS": "*"},
-        "Action":["s3:GetObject"],
-        "Resource":["arn:aws:s3:::${var.domain}/*"]
-      }]
-    }
-  EOF
 }
 
 resource "aws_s3_bucket_website_configuration" "site_config" {
