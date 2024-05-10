f47357811997a42c30c5184ffbeadafc2dfa94af
Eric Wehrly <1851833+EricWehrly@users.noreply.github.com>
2024-05-10 01:59:15 -0400

    add support for mjs

--- modules/s3-site/files.tf
+++ modules/s3-site/files.tf
@@ -4,8 +4,11 @@ module "template_files" {
   source = "hashicorp/dir/template"
 
   base_dir = "${var.filedir}"
-  template_vars = {
-    # Pass in any values that you wish to use in your templates.
+  
+  file_types = {
+    # defaults 
+    ".3g2": "video/3gpp2", ".3gp": "video/3gpp", ".atom": "application/atom+xml", ".css": "text/css; charset=utf-8", ".eot": "application/vnd.ms-fontobject", ".gif": "image/gif", ".htm": "text/html; charset=utf-8", ".html": "text/html; charset=utf-8", ".ico": "image/vnd.microsoft.icon", ".jar": "application/java-archive", ".jpeg": "image/jpeg", ".jpg": "image/jpeg", ".js": "application/javascript", ".json": "application/json", ".jsonld": "application/ld+json", ".otf": "font/otf", ".pdf": "application/pdf", ".png": "image/png", ".rss": "application/rss+xml", ".svg": "image/svg+xml", ".swf": "application/x-shockwave-flash", ".ttf": "font/ttf", ".txt": "text/plain; charset=utf-8", ".weba": "audio/webm", ".webm": "video/webm", ".webp": "image/webp", ".woff": "font/woff", ".woff2": "font/woff2", ".xhtml": "application/xhtml+xml", ".xml": "application/xml",
+    ".mjs": "text/javascript"
   }
 }
 
